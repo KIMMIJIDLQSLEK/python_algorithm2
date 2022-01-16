2f52c551d66f43b4903173d790801028b7c96367
MIJI <96563260+KIMMIJIDLQSLEK@users.noreply.github.com>
2022-01-16 23:48:01 +0900

    find_random_binary

---
+++ 08_is_existing_target_random_binary.py
@@ -0,0 +1,10 @@
+finding_target = 2
+finding_numbers = [0, 3, 5, 6, 1, 2, 4]  #이진탐색을 할경우 정렬한후 해야함
+
+def is_exist_target_number_binary(target, numbers):
+    # 이 부분을 채워보세요!
+    return 1
+
+
+result = is_exist_target_number_binary(finding_target, finding_numbers)
+print(result)
